b7b232e7c039400bd57c0fea62ccf14a09fe1bdf
ShosukeDoi <doiverson.canada@gmail.com>
2020-02-28 13:22:30 -0800

    update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // key.js -- figure out what set of credentials to return
-if (process.env.NODE_DEV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     module.exports = require('./dev');
