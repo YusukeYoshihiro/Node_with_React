a2f946f989b5553e6855ca1275ad0d3bbdcba9f5
ShosukeDoi <doiverson.canada@gmail.com>
2020-02-28 12:40:37 -0800

    update

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,2 @@
-module.exports = {
-  googleClientID: '1069481510958-234n6eue8a137909pjcg5n3a05n1cq5k.apps.googleusercontent.com',
-  googleClientSecret: '8HtLiSBrTDjnQJmcvmwYBgSf'
-};
+// key.js -- figure out what set of credentials to return
+
